5eb1c52dd0d40ec6a47f945698e1267819f52856
Yuichiro Iwai <yuichiro.iwai@gmail.com>
2019-07-01 08:38:07 +0900

    layerの下準備

--- core/src/main/scala/com/yuiwai/kasumi/core/concept/LayerOps.scala
+++ core/src/main/scala/com/yuiwai/kasumi/core/concept/LayerOps.scala
@@ -1,5 +1,13 @@
 package com.yuiwai.kasumi.core.concept
 
-trait LayerOps {
-
+sealed trait LayerOps {
+  type Data
+}
+trait NodeLayerOps extends LayerOps {
+  type N <: NodeOps[_]
+  def find(node: N): Option[Data]
+}
+trait EdgeLayerOps extends LayerOps {
+  type E <: EdgeOps[_]
+  def find(edge: E): Option[Data]
 }
--- core/src/test/scala/com/yuiwai/kasumi/core/implementation/LayerSpec.scala
+++ core/src/test/scala/com/yuiwai/kasumi/core/implementation/LayerSpec.scala
@@ -4,6 +4,12 @@ import utest._
 
 object LayerSpec extends TestSuite {
   val tests = Tests {
+    val board = Board.empty ~ (1, 2) ~ (2, 3) ~ (2, 4) ~ (1, 3)
+    "node layer" - {
 
+    }
+    "edge layer" - {
+
+    }
   }
 }
